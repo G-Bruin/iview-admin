88f9de225d24b8e60398abb1c755c41a81e6be47
G-Bruin <814878862@qq.com>
2021-06-07 22:25:17 +0800

    增加页面

--- src/components/officeBuild/userAdd.vue
+++ src/components/officeBuild/userAdd.vue
@@ -1,26 +1,63 @@
 <template>
-  <Form :model="formItem" :label-width="80">
-    <FormItem label="账号">
-      <Input v-model="formItem.username" placeholder="请输入账号"></Input>
+  <Form ref="formValidate" :model="formValidate" :rules="ruleValidate"  :label-width="80">
+    <FormItem label="账号" prop="username">
+      <Input v-model="formValidate.username" placeholder="请输入账号"></Input>
     </FormItem>
 
-    <FormItem label="密码">
-      <Input v-model="formItem.password" type="password" placeholder="请输入密码"></Input>
+    <FormItem label="密码" prop="password">
+      <Input v-model="formValidate.password" type="password" placeholder="请输入密码"></Input>
     </FormItem>
     <FormItem>
-      <Button type="primary">提交</Button>
+      <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
+      <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
     </FormItem>
   </Form>
 
 </template>
 <script>
+  // import axios from '@/libs/api.request'
+
   export default {
     data () {
       return {
-        formItem: {
-          password: '',
-          username: ''
+        tableData: [],
+        formValidate: {
+          username: '',
+          password: ''
+        },
+        ruleValidate: {
+          username: [
+            { required: true, message: '请输入账号', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
+          ]
         }
+      }
+    },
+    methods: {
+      handleSubmit (name) {
+        // let _this = this
+        // let formValidate = _this.formValidate
+
+        // axios.get('/user?ID=12345')
+        //   .then(function (response) {
+        //     console.log(response)
+        //   })
+        //   .catch(function (error) {
+        //     console.log(error)
+        //   })
+
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            this.$Message.success('Success!')
+          } else {
+            this.$Message.error('Fail!')
+          }
+        })
+      },
+      handleReset (name) {
+        this.$refs[name].resetFields()
       }
     }
   }
